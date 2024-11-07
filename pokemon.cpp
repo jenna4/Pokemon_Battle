f6a8273a213619f01e1c18b3f0d7e1a77b2f1f17
Jenna Rivera <j3nna.riv@gmail.com>
2024-11-06 21:42:08 -0800

    wtf

--- pokemon.cpp
+++ pokemon.cpp
@@ -1,1 +1,67 @@
 // TODO Implement member functions of the pokemon class
+#include "pokemon.hpp"
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <cmath>
+
+
+// pokemon::pokemon (int size) {
+// this->num_moves = size;
+// this->move_arr = new move [size];
+// }
+
+pokemon::pokemon (string& name, string& type, Stats& stats, string& move_type, int damage, int uses) {
+    this->name = name;
+    this->type = type;
+    this->stats = stats;
+    this->hp = stats.starting_hp;
+    this->num_moves = num_moves;
+
+    this->move_arr = new move[num_moves];
+}
+
+pokemon::~pokemon() {
+    delete[] move_arr;
+    move_arr = nullptr;
+}
+
+int pokemon::prompt_move_heal() {
+    int mhchoice;
+    cout << "Would you like to 1) use a move, or 2) heal?: ";
+    cin >> mhchoice;
+    return mhchoice;
+}
+
+string pokemon::prompt_move() {
+    string move_choice;
+    cout << "Which move would you like to use?";
+    for (int i = 0; i < this->num_moves; i++) {
+        this->move_arr[i].display_moves();
+    }
+}
+
+
+pokemon& pokemon::operator=(const pokemon &other) {
+    if (this != &other) {
+        delete[] move_arr;
+        num_moves = other.num_moves;
+        move_arr = new move[num_moves];
+        for (int i = 0; i < num_moves; i++) {
+            move_arr[i] = other.move_arr[i];
+        }
+    }
+    return *this;
+}
+
+pokemon::pokemon(const pokemon &other) {
+    num_moves = other.num_moves;
+
+    move_arr = new move[num_moves];
+
+    for (int i = 0; i < num_moves; i++) {
+        move_arr[i] = other.move_arr[i];
+    }
+    // pokemon p1;
+	// p1.prompt_move();
+}
--- pokemon.hpp
+++ pokemon.hpp
@@ -1,8 +1,40 @@
 #ifndef POKEMON_HPP
 #define POKEMON_HPP
+#include "move.hpp"
+#include <iostream>
+#include <string> 
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+// using namespace std;
+
+//creat sruct for stats
+struct Stats {
+	int attack;
+	int defense;
+	int starting_hp;
+};
 
 class pokemon {
-	// TODO
+	string name;
+	string type;
+	Stats stats;
+	int hp;
+	move* move_arr; 
+	int num_moves;
+
+	public: 
+	pokemon(string& name, string& type, Stats& stats, string& move_type, int damage, int uses);
+	~pokemon(); 
+	pokemon& operator=(const pokemon &other); 
+	pokemon(const pokemon &other);
+	int prompt_move_heal();
+	string prompt_move();
 };
 
 #endif
