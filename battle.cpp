66a9e20ef00e64158859d2188daec37db949df40
Jenna Rivera <j3nna.riv@gmail.com>
2024-11-06 21:43:45 -0800

    hmm

--- battle.cpp
+++ battle.cpp
@@ -1,1 +1,25 @@
 // TODO Implement member functions of the battle class
+#include <iostream>
+#include <string> 
+#include "pokemon.hpp"
+#include "battle.hpp"
+// prompt user for move
+int prompt_move_heal() {
+    int mhchoice;
+    cout << "Would you like to 1) use a move, or 2) heal?: ";
+    cin >> mhchoice;
+    return mhchoice;
+}
+
+string prompt_move() {
+    string move_choice;
+    cout << "Which move would you like to use?";
+}
+
+// way to calculate damage done by move 
+
+// apply damage to opps pokemon
+
+// if damage is greater than hp 
+
+// healing potion 
